b91f8d4bb868065c7e0bbcb813d4181cda766f0c
bhagoliyritik-design <bhagoliyajitendra@gmail.com>
2026-07-30 17:15:12 +0530

    final typescript peer initiator strict fix

--- src/components/VoiceChatWindow.tsx
+++ src/components/VoiceChatWindow.tsx
@@ -89,9 +89,11 @@ export default function VoiceChatWindow({
       }
 
       peerRef.current = new Peer({
-  initiator: socket && socket.id && partnerId
-    ? socket.id < partnerId
-    : false,
+  initiator:
+    typeof socket?.id === "string" &&
+    typeof partnerId === "string"
+      ? socket.id < partnerId
+      : false,
   trickle: false,
   stream,
       });
